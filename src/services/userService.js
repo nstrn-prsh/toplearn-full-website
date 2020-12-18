adb5fa54e079e31da369f30fdac76d614b51c1e7
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2020-12-18 15:19:17 +0330

    e15.9- offline api :)

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,9 +2,11 @@ import config from "./config.json";
 import http from "./httpService";
 
 export const registerAxios = (user) => {
-  return http.post(`${config.toplearnapi}/api/register`, JSON.stringify(user));
+  // return http.post(`${config.toplearnapi}/api/register`, JSON.stringify(user));
+  return http.post(`${config.localapi}/api/register`, JSON.stringify(user));
 };
 
 export const loginAxios = (user) => {
-  return http.post(`${config.toplearnapi}/api/login`, JSON.stringify(user));
+  // return http.post(`${config.toplearnapi}/api/login`, JSON.stringify(user));
+  return http.post(`${config.localapi}/api/login`, JSON.stringify(user));
 };
