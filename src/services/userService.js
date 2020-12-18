484088f3fa0a37fd5021dbbaca8917ba54815971
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2020-12-18 18:40:35 +0330

    final-1

--- src/services/userService.js
+++ src/services/userService.js
@@ -10,3 +10,14 @@ export const loginAxios = (user) => {
   // return http.post(`${config.toplearnapi}/api/login`, JSON.stringify(user));
   return http.post(`${config.localapi}/api/login`, JSON.stringify(user));
 };
+
+
+// baraye kar ba local host aval mongodb ro nasb mikonim
+// bad mongo compass ro nasb mikonim
+// (service mongodb bayad dar hale running bashe 
+//   az windows+r Service.msc mishe check kard)
+// bad toye masire server npm start ro mizanim
+// ke port servero behemoon mide
+// (age posheye node-modules nabood aval npm i)
+// vaghti server dar hale run bashe proje ham run mikonim
+// hala mishe sabte nam kard va varede account shod 
