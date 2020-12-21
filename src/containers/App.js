23bc911b1e292ae7a5acb89836931da5c83ab4fd
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2020-12-21 20:06:43 +0330

    e17.4- jquery in react

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import TopLearn from "./TopLearn";
 
 const App = () => {
+  useEffect(() => {
+    require('./../utils/script')
+  }, []);
   return (
     <Router>
       <TopLearn />
